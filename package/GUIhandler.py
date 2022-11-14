5ee48cb0a200b0f96e3cdb9fc37feeb554b51a0f
Chandrodoy Saha <65346836+Cs-17@users.noreply.github.com>
2022-11-14 14:30:40 +0530

    set gui parameters

--- package/GUIhandler.py
+++ package/GUIhandler.py
@@ -1,12 +1,36 @@
-import tkinter as t
 from tkinter import *
-from tkinter import messagebox
-from tkinter import Label
-from tkinter import ttk
-from tkinter import PhotoImage
-from tkinter import Frame
-global wndw
-def makeWindow():
+import tkinter
+window=Tk()
+window.title("Mountain Stores")
+
+x_axis=(window.winfo_screenwidth())
+y_axis=(window.winfo_screenheight())
+
+winx=int(x_axis*0.7)
+winy=int(y_axis*0.7)
+
+window.geometry("{}x{}".format(winx,winy))
+window.resizable(False,False)
+window.attributes('-topmost', 1)
+#bgimg=tkinter.PhotoImage(file="./EXTRAS/logo.jpg")
+#l1=Label(window,i=bgimg)
+#l1.pack()
+window.configure(bg='yellow')
+window.iconbitmap('./EXTRAS/logo.ico')
+window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+"""
+  makeWindow():
     print()
     wndw=t.Tk()
     wndw.title("Shop Manager")
@@ -17,8 +41,9 @@ def makeWindow():
     l=Label(wndw,text="Initialisation complete",font=(universalFont,20),bg=universalBg,fg="#8A307F", ).pack(side="top",padx=10,pady=10)
     #wndw.mainloop()
 def done():
+
     messagebox.showinfo("showinfo", "Information")
-'''def makeWindow():
+def makeWindow():
     print()
     global wndw
     wndw=t.Tk()
@@ -26,4 +51,5 @@ def done():
     global universalBg,universalFont
     universalBg="#8399c9"
     universalFont="Century Gothic"
-    wndw.config(bg=universalBg)'''
+    wndw.config(bg=universalBg)
+    """
